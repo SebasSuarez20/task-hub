85c45b16a2c60b05a6f6e3fdad240d02b4b034d1
Juan Sebastian <93970245+SebasSuarez20@users.noreply.github.com>
2026-04-25 00:43:27 -0500

    feat: Add CORS configuration for handling cross-origin requests

---
+++ franchise-service/franchise-service/src/main/java/com/sales/franchise/infraestructure/config/CorsConfig.java
@@ -0,0 +1,23 @@
+package com.sales.franchise.infraestructure.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+    @Configuration
+public class CorsConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:4200") // en este caso como no se va a hacer front-end
+                        .allowedMethods("*");
+            }
+        };
+    }
+}
+
